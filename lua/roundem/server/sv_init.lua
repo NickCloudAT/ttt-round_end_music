74e31a0aea4b3326a02f448137d9efdccb99ff1e
TheNickSkater <nicolasbeiglboeck@gmx.at>
2020-04-13 01:06:04 +0200

    Changed file structure

--- lua/roundem/server/sv_init.lua
+++ lua/roundem/server/sv_init.lua
@@ -2,6 +2,7 @@ ROUNDEM_DATA = {}
 ROUNDEM_DATA.SOUNDS = {}
 ROUNDEM_DATA.ALLOWED_FILES = {"mp3", "ogg", "wav"}
 
+include("sv_setup.lua")
 include("sv_handler.lua")
 
 util.AddNetworkString("TTTRoundEndMusic_Start")
